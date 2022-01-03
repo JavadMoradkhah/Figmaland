f05726838fc4c0660d3e6df0df9f59656abd414e
JavadMh <javadprogrammer10@gmail.com>
2022-01-03 17:06:31 +0330

    Compress images, make svg sprite and cleanup markup.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-/* Variables */
-let lastScrollTop = 0;
-
 /* DOM Elements */
 const Header = document.getElementById("app-header");
 const navToggler = document.getElementById("nav-toggler");
@@ -9,12 +6,12 @@ const navigationMenu = document.getElementById("navigation-menu");
 /* Navigation Menu Functions */
 function openNavigationMenu() {
   navigationMenu.classList.add("expanded");
-  navToggler.src = "/assets/icon/close.svg";
+  navToggler.innerHTML = `<use href="/assets/icon/icon-sprite.svg#close"></use>`;
 }
 
 function closeNavigationMenu() {
   navigationMenu.classList.remove("expanded");
-  navToggler.src = "/assets/icon/menu.svg";
+  navToggler.innerHTML = `<use href="/assets/icon/icon-sprite.svg#menu"></use>`;
 }
 
 /* Window Events */
@@ -30,12 +27,11 @@ window.onload = function () {
 
 window.onscroll = function () {
   const st = window.pageYOffset || document.documentElement.scrollTop;
-  if (st > lastScrollTop) {
+  if (st > 0) {
     Header.style.boxShadow = "10px 0 20px rgba(0,0,0,0.4)";
   } else {
     Header.style.boxShadow = null;
   }
-  lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 };
 
 window.onclick = function (e) {
